c8f7f8d57f54be2296578e521d1906583d87ed71
max_lian <135724689z>
2020-09-23 18:00:28 +0300

    Initial commit

---
+++ src/main/java/yakovlev/m/converter/model/Student.java
@@ -0,0 +1,8 @@
+package yakovlev.m.converter.model;
+
+public class Student {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+}
